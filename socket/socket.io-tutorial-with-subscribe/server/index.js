34180ae94480d451690138b54936ff6d470096da
Ethan Strominger <ethanstrominger2@gmail.com>
2020-06-10 16:29:15 -0400

    Move directories for tutorial

--- socket/socket.io-tutorial-with-subscribe/server/index.js
+++ socket/socket.io-tutorial-with-subscribe/server/index.js
@@ -1,9 +1,10 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.resolve('../client/index.html'));
 });
 
 io.on('connection', (socket) => {
